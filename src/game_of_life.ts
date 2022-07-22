3de22510a305000d91a65dc2f931071e020741ec
Jesse Govindan <jesse.govindan@chillisoft.co.za>
2022-07-22 15:00:38 +0200

    Include js-test and first test

--- src/game_of_life.ts
+++ src/game_of_life.ts
@@ -1,2 +1,21 @@
+export enum Cell {
+  Dead,
+  Alive,
+}
+export type Board = Cell[][]
+
 export class GameOfLife {
+  constructor(initialPattern: Board) {
+  }
+
+  evolve() {
+  }
+
+  getBoard(): Board {
+    return [
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+    ]
+  }
 }
--- src/test/game_of_life.test.ts
+++ src/test/game_of_life.test.ts
@@ -1,2 +1,24 @@
+import {Board, Cell, GameOfLife} from '../game_of_life'
+
 describe('GameOfLife', () => {
+  test('an alive cell dies when surround only by dead cells', () => {
+    // Arrange
+    const game = new GameOfLife(fromTestBoard([
+      ['O', 'O', 'O'],
+      ['O', 'O', 'X'],
+      ['O', 'O', 'X'],
+    ]))
+    // Act
+    game.evolve()
+    // Assert
+    expect(game.getBoard()).toEqual(fromTestBoard([
+      ['O', 'O', 'O'],
+      ['O', 'O', 'O'],
+      ['O', 'O', 'O'],
+    ]))
+  })
 })
+
+function fromTestBoard(board: ('X' | 'O')[][]): Board {
+  return board.map(row => row.map(cell => cell === 'X' ? Cell.Alive : Cell.Dead))
+}
